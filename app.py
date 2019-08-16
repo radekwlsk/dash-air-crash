ff854befbe83f22d794e64300e629373297b0b26
Radosław Kowalski <radek.kwlsk@gmail.com>
2019-08-16 16:13:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='Airplane Crashes Data - Dash'),
+
+    html.Div(children='''
+        Airplane crashes data visualisation project for purposes of presentation about Dash
+    '''),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
