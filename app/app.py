60b388143df3fbab65fb9df4beeabcc86d888b80
radoslaw.kowalski <radoslaw.kowalski@10clouds.com>
2019-08-18 12:12:59 +0200

    use few html tags

--- app/app.py
+++ app/app.py
@@ -4,13 +4,31 @@ import dash_html_components as html
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(children=[
-    html.H1(children='Airplane Crashes Data - Dash'),
+app.title = "Airplane Crashes Data - Dash"
+layout = [
+    html.Header(children=[
+        html.H1(app.title),
+        html.P("Airplane crashes data visualisation project for purposes of presentation about Dash"),
+    ]),
 
-    html.Div(children='''
-        Airplane crashes data visualisation project for purposes of presentation about Dash
-    '''),
-])
+    html.Hr(),
+
+    html.Section(children=[
+            html.H2('Data Source'),
+            html.A(
+                'Full history of airplane crashes throughout the world, from 1908-present.',
+                href='https://opendata.socrata.com/Government/Airplane-Crashes-and-Fatalities-Since-1908/q2te-8cvq'
+            ),
+            html.P("Data details:"),
+            html.Ul(children=[
+                html.Li("Total rows: 5268"),
+                html.Li("Source Domain: opendata.socrata.com"),
+                html.Li("Created: 24/06/2009, 18:35:20"),
+            ])
+    ]),
+]
+
+app.layout = html.Div(layout, className='container')
 
 if __name__ == '__main__':
     app.run_server(host="0.0.0.0", debug=True)
