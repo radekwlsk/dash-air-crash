9b20870abbe637234f2f886c6465589c53394d31
radoslaw.kowalski <radoslaw.kowalski@10clouds.com>
2019-08-30 09:20:19 +0200

    dataset: present in html table

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+from utils import socrata
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -16,7 +18,7 @@ header = html.Header(children=[
 data_source_url = 'https://opendata.socrata.com/Government/Airplane-Crashes-and-Fatalities-Since-1908/q2te-8cvq'
 data_source_section = dcc.Markdown(f"""
 ## Data Source
-[Full history of airplane crashes throughout the world, from 1908-present.]({data_source_url})
+[Full history of airplane crashes throughout the world, from 1908-2009.]({data_source_url})
 
 Data details:
   - Total rows: 5268
@@ -24,11 +26,31 @@ Data details:
   - Created: 24/06/2009, 18:35:20
 """)
 
+df = socrata.get_air_crashes(limit=100)
+air_crashed_table = html.Table(children=[
+    html.Tr([
+        html.Th("Date"),
+        html.Th("Location"),
+        html.Th("Fatalities"),
+        html.Th("Registration"),
+        html.Th("Operator"),
+    ]),
+    *[html.Tr([
+        html.Td(row['date']),
+        html.Td(row['location']),
+        html.Td(row['fatalities']),
+        html.Td(row['registration']),
+        html.Td(row['operator']),
+    ]) for row in df.to_dict('records')]
+])
+
 layout = [
     header,
-    data_source_section,
+    html.Div(children=[
+        data_source_section,
+        air_crashed_table,
+    ]),
 ]
-
 app.layout = html.Div(layout, className='container')
 
 if __name__ == '__main__':
---
+++ app/utils/socrata.py
@@ -0,0 +1,24 @@
+from os import getenv
+
+import pandas as pd
+from sodapy import Socrata
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+client = Socrata(getenv('SOCRATA_API_URL'), None)
+
+
+def get_air_crashes(limit=2000, raw=False):
+    results = client.get(
+        getenv('SOCRATA_AIRCRASHES_DATASET_ID'),
+        limit=limit,
+        order='date DESC',
+    )
+    if raw:
+        return results
+    else:
+        # Convert to pandas DataFrame
+        df = pd.DataFrame.from_records(results)
+        df['date'] = pd.to_datetime(df['date'], unit='s').dt.date
+        df['fatalities'] = pd.to_numeric(df['fatalities'])
+        return df
